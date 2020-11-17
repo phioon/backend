bf33fc7a610bee4de6db6e6b582ec2de8454e3a4
fcoouto <fcoouto@users.noreply.github.com>
2020-11-17 13:51:07 +0100

    [GAE] Warmup Requests

--- django_engine/urls.py
+++ django_engine/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-
+from . import views
 
 urlpatterns = [
     path('api/market/', include('market.urls')),
-    path('api/provider/', include('provider.urls'))
+    path('api/provider/', include('provider.urls')),
+    path('_ah/warmup/', views.warmup, name='gae_warmup_request')
 ]
